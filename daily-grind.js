dd66d36e9b2f12ee3cbc0bfb0ae3a983ba860006
sadiestevenson <93157504+sadiestevenson@users.noreply.github.com>
2022-07-06 09:17:53 -0700

    Update daily-grind.js

--- daily-grind.js
+++ daily-grind.js
@@ -1,1 +1,33 @@
-alert("ITs working!");
+//alert("ITs working!");
+
+
+/* 
+Here are the items we need to change for each coffee: 
+image-src of pic
+alt- alt tag og pic
+description-  a paragraph of content about the coffee
+name of today's coffee
+color representing coffee
+ today the current day of the week
+ */
+
+
+let myDate = new Date ();
+let myDay = myDate.getHours ();
+
+Switch(myDa){
+    
+  case 0:
+    today = "Sunday";
+    break;
+    
+  case 2:
+    today = "Tuesday";
+  break;
+  
+  default:
+  today ="Something went weong!";
+  
+}
+
+alert(today);
